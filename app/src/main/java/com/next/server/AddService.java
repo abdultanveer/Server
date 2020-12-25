d7a47220d9f2a603ac676fda6ff483a5a38d5587
abdul <abdul.tanveer@gmail.com>
2020-12-25 08:01:58 +0530

    added intent filter in manifest for client to invoke service

--- app/src/main/java/com/next/server/AddService.java
+++ app/src/main/java/com/next/server/AddService.java
@@ -3,14 +3,22 @@ package com.next.server;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
+import android.os.RemoteException;
 
 public class AddService extends Service {
     public AddService() {
     }
 
+    private AddAidl.Stub mBinder = new AddAidl.Stub() {
+        @Override
+        public int add(int a, int b) throws RemoteException {
+            return a+b;
+        }
+    };
+
     @Override
     public IBinder onBind(Intent intent) {
         // TODO: Return the communication channel to the service.
-        throw new UnsupportedOperationException("Not yet implemented");
+      return  mBinder;
     }
 }
